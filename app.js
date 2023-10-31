3062462f2460a5b94d7b1a0011152e206fa6c81a
v3n0m97 <111784478+v3n0m97@users.noreply.github.com>
2023-10-31 18:27:41 +0200

    asd
    asd

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send("done");
+});
+
+app.listen(4000, () => {
+  console.log('server connected');
+});
